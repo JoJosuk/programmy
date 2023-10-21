2ac7dc45a6b3c0b59e2379341ab9cac818173d2e
Joel J George <joeljoby111@gmail.com>
2023-10-21 10:18:13 +0530

    feat: ace editor trying

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,16 @@ import React, { Component } from "react";
 // import { render } from "react-dom";
 import AceEditor from "react-ace";
 import "brace/mode/javascript";
-import "brace/theme/tomorrow";
+import "brace/theme/monokai";
 
 export default class Main extends Component {
   render() {
     return (
-      <div>
-        <AceEditor mode="javascript" theme="tomorrow" />
-      </div>
+      <AceEditor
+        mode="javascript"
+        theme="monokai"
+        className="w-screen h-screen"
+      />
     );
   }
 }
